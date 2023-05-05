5cadd17336ae12dac44be5ba9a1a47a404df6ee4
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-05-05 23:14:37 +0900

    Q3

---
+++ q3/work/js/app.js
@@ -0,0 +1,18 @@
+$(function () {
+  //drawer_buttonをクリックすると
+  $(".drawer_button").on("click", function () {
+    //ボタンにactiveクラスを付与
+    $(this).toggleClass("active");
+    //ナビにopenクラスを付与
+    $("nav").toggleClass("open");
+    //.drawer_bgをフェードインして表示
+    $(".drawer_bg").fadeToggle()
+  });
+  //drawer_bgをクリックすると
+  $(".drawer_bg").on("click", function () {
+    //ボタンに付与していたactiveクラスを解除
+    $(".drawer_button").removeClass("active");
+    //ナビに付与していたopenクラスを解除
+    $("nav").removeClass("open")
+  });
+});

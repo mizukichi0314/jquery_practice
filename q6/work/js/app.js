81317ebb4fe569618cdf062a7a4d7a76e6580806
m.hashiba <118049319+mizukichi0314@users.noreply.github.com>
2023-05-09 12:18:44 +0900

    Q6

---
+++ q6/work/js/app.js
@@ -0,0 +1,31 @@
+$(function () {
+  //select-boxの要素選択時にchangeイベントが発生
+  $(".select-box").on("change", function () {
+    //selectの変数に、選択されたselect-box内の値を取得
+    const select = $(this).val();
+    //変数の値がallだった場合
+    if (select === "all") {
+      //リストの値を全て表示
+      $(".food-list li").show();
+      //select-boxの要素選択時にfood-list内でループ処理を加える
+      $(".food-list li").each(function () {
+        //food-list内の項目を表示
+        $(this).show();
+      });
+      //上記以外の場合
+    } else {
+      //select-boxの要素選択時にfood-list内でループ処理を加える
+      $(".food-list li").each(function () {
+        //food-list内の値と、data-category-typeの値が一致した時の処理
+        if ($(this).data("category-type") === select) {
+          //food-list内の項目表示
+          $(this).show();
+          //上記以外の場合
+        } else {
+          //food-list内の項目非表示
+          $(this).hide();
+        };
+      });
+    };
+  });
+});
